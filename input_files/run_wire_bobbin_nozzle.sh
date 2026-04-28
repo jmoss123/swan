4db01caacc8317635f25c16dd9a955c05f670e02
jmoss123 <joemoss15@outlook.com>
2026-04-28 16:26:31 +0100

    Adjust slurm script to ignore warnings when running MOOSE inputs

---
+++ input_files/run_wire_bobbin_nozzle.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=wire_bobbin_nozzle
+#SBATCH --output=%x_%j.out        
+#SBATCH --error=%x_%j.err         
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8       
+#SBATCH --mem=32G                  
+#SBATCH --time=12:00:00            
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=jpamoss1@sheffield.ac.uk
+
+# Module environment
+module purge
+module restore moose
+
+# Conda Environment 
+source ~/.bashrc
+conda activate moose
+
+# Move to the directory containing the input file
+cd /users/mea21jpm/projects/swan/input_files
+
+# Logging
+echo "Job started: $(date)"
+echo "Running on node: $(hostname)"
+echo "MPI ranks: $SLURM_NTASKS"
+
+# Run
+mpirun -n $SLURM_NTASKS /users/mea21jpm/projects/swan/swan-opt -i wire_bobbin_nozzle_combined.i -w --n-threads=1
+
+echo "Job finished: $(date)"
